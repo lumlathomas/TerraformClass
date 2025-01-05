0ebe6d10d2b99959653c42b7b7cfbea91d1cee57
Lumla Thomas <lumlarose@gmail.com>
2025-01-05 00:30:50 +0000

    Day-4 dyanamodb and s3 bucket added

--- Day-3-statefile/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "lumlabucket"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
--- Day-4-Terraform-Statefile-Locking/main.tf
+++ Day-4-Terraform-Statefile-Locking/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test" {
   ami               = "ami-019374baf467d6601"
   instance_type     = "t2.micro"
   key_name          = "LondonKP"
-  availability_zone = "eu-west-2b"
+  availability_zone = "eu-west-2a"
   tags = {
     Name = "TerrformTest"
   }
