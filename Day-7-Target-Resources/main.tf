1b91ae3a4d9a53aff52e78ceda12af41aa9c3d7c
Lumla Thomas <lumlarose@gmail.com>
2025-01-07 22:06:34 +0000

    terraform target resource option done

---
+++ Day-7-Target-Resources/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "dev" {
+    ami = "ami-019374baf467d6601"
+    instance_type = "t2.micro"
+    availability_zone = "eu-west-2a"
+    
+  tags = {
+    Name = "ec2-instance"
+  }
+}
+
+resource "aws_s3_bucket" "dependent" {
+    bucket = "lumlaytvhshfbbc" 
+}
+
+#terraform apply -target=aws_instance.dev
+#terraform destroy -target=aws_instance.dev
+#terraform apply -target=aws_s3_bucket.dependent
+#terraform destroy -target=aws_s3_bucket.dependent
