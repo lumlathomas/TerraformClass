568ace65b98ed5b5524b1cf09b4a47832b026a70
Lumla Thomas <lumlarose@gmail.com>
2025-01-08 21:07:01 +0000

    Update main.tf

--- Day-8-Module-GitHub-Source/main.tf
+++ Day-8-Module-GitHub-Source/main.tf
@@ -1,5 +1,5 @@
 module "prod" {
-  source = "github.com/lumlathomas/TerraformClass/Day-8-EC2Creation-Template"
+  #source = "github.com/lumlathomas/TerraformClass/Day-8-EC2Creation-Template"
   ami_id = "ami-019374baf467d6601"
   instance_type = "t2.micro"
   key_name = "LondonKP"
